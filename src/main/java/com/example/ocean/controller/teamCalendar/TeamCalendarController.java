caa69f69e6dbb7950b0e773633c59262de7d6486
sumni-125 <dltnalssumin@gmail.com>
2025-07-03 15:16:48 +0900

    메인페이지, 개인캘린더, 팀캘린더 연결, 팀캘린더 프론트는 아직x 개인캘린더 프론트 수정필요

--- src/main/java/com/example/ocean/controller/teamCalendar/TeamCalendarController.java
+++ src/main/java/com/example/ocean/controller/teamCalendar/TeamCalendarController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TeamCalendarController {
 
-    @GetMapping("/teamCalendar")
+    @GetMapping("/calendar/team")
     public String hihi(){
         return "calendar";
     }
